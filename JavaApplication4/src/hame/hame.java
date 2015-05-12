4ce54b664b52f4ba9f9cd03d4fbeefa734cd5b91
redlikedeath <charles.bonn@mines.sdsmt.edu>
2015-05-12 12:00:06 -0600

    first upload

---
+++ JavaApplication4/src/hame/hame.java
@@ -0,0 +1,16 @@
+package hame;
+
+//imports
+import javax.swing.*;
+
+public class hame
+{
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame("The Hame");
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
+
---
+++ JavaApplication4/src/hame/hero.java
@@ -0,0 +1,46 @@
+package hame;
+
+class hero
+{
+  //character data.
+  private String name;
+  private String title;
+  private boolean gender;
+  //flexable stats
+  private int health;
+  private int mana;
+  //leveling stats
+  private int level;
+  private int experience;
+  //offensive stats
+  private int attack_power;
+  private int accuracy;
+  private int attack_speed;
+  //offensive magic stats
+  private int fire_magic;
+  private int water_magic;
+  private int earth_magic;
+  private int air_magic;
+  //defensive stats
+  private int defense;
+  private int physical_resistance;
+  private int fire_resistance;
+  private int water_resistance;
+  private int air_resistance;
+  private int earth_resistance;
+  
+  //other stats
+  private int movement_speed;
+  
+  private boolean isDead = false;
+  
+  public void melee_attack(hero ef)
+  {
+      
+  }
+  
+  public void defend(hero ef)
+  {
+      
+  }
+}
